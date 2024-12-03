ad99c3f64d1f7dfd1d817fffb3d6e65d4da4c556
Guillermo Gallo <memogallo@gmail.com>
2024-12-02 20:38:53 -0500

    improving readme

--- src/utils/logging_utils.py
+++ src/utils/logging_utils.py
@@ -3,7 +3,6 @@ import logging
 
 def get_log_level(log_level_name: str):
     log_level_name = log_level_name.lower()
-    print(log_level_name)
     if log_level_name == "debug":
         return logging.DEBUG
     elif log_level_name == "info":
